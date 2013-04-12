23fe8fec4c7edf4447251f80d7f41198390ba001
Nikita Prokopov <prokopov@gmail.com>
2013-04-12 19:43:46 +0700

    README update

--- src/someapp/server.clj
+++ src/someapp/server.clj
@@ -3,20 +3,10 @@
 
 (def env (into {} (System/getenv)))
 
-; (defn async-handler [ring-request]
-;   ;; unified API for WebSocket and HTTP long polling/streaming
-;   (server/with-channel ring-request channel    ; get the channel
-;     (if (server/websocket? channel)            ; if you want to distinguish them
-;       (server/on-receive channel (fn [data]     ; two way communication
-;                                   (server/send! channel data)))
-;       (server/send! channel {:status 200
-;                       :headers {"Content-Type" "text/plain"}
-;                       :body    "Long polling?"}))))
-
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "hello from http-kit v2!"})
+   :body    "Hello from http-kit!"})
 
 (defn -main [& args]
   (server/run-server app { :ip (env "HOST") :port (Integer/parseInt (env "PORT")) }))
